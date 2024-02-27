99d624a5b3db2f262ca867c959c7866cd6c089b2
Sagar Ch <sagarch700@gmail.com>
2024-02-27 23:51:35 +0530

    converting the aggregated, multiresult to python dictionary for parsing

--- Nornir_Automation/runbook_napalm.py
+++ Nornir_Automation/runbook_napalm.py
@@ -5,8 +5,18 @@ from nornir_utils.plugins.functions import print_result
 nr = InitNornir(config_file="config.yml")
 
 def pull_info(task):
-    task.run(task = napalm_get, getters = ["get_facts"])
+    result = task.run(task = napalm_get, getters = ["get_interfaces"])
 
 results = nr.run(task = pull_info)
-print_result(results)
+
+# Parsing the data is tricky, so follow the below steps
+# nornir gives and aggregated result, to get the result of a specific device use its hostname and [1] .result converts the output to string
+parsing = results["R4"][1].result
+# get the dict of all the interfaces
+interface_parse = parsing["get_interfaces"]
+# get the outptut of a specific interface you like
+interface_data = interface_parse["GigabitEthernet0/0/0/0"]
+print(interface_data)
+# print_result(results)
+
 
