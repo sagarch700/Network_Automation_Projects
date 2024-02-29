c0a17ae2d15e5ddeaec18007f3ca7b5602ba9dad
Sagar Ch <sagarch700@gmail.com>
2024-02-29 23:40:29 +0530

    Archiving network configuration and state

--- network_backup/runbook1.py
+++ network_backup/runbook1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from nornir import InitNornir
 from nornir_scrapli.tasks import send_command, send_configs, send_commands, send_commands_from_file, send_configs_from_file
 from nornir_utils.plugins.tasks.files import write_file
@@ -7,12 +8,22 @@ import pathlib
 
 nr = InitNornir(config_file= "config.yml")
 
+command_list = []
+user_input = input("Enter the commands and press enter if no futher command: ")
+while True:
+    command_list.append(user_input)
+    user_input = input("Enter the commands and press enter if no futher command: ")
+    if user_input == "\n":
+        break
+
 def backup_config(task):
-    cmds = ["show run", "show version"]
-    for cmd in cmds:
+    # cmds = ["show run", "show version"]
+    for cmd in command_list:
+        folder = cmd.replace(" ", "-")
+        folder = folder.strip("\n")
         config_dir = "config-archive"
         date_dir = config_dir + "/" + str(date.today())
-        command_dir = date_dir + "/" + cmd
+        command_dir = date_dir + "/" + folder
         pathlib.Path(config_dir).mkdir(exist_ok= True)
         pathlib.Path(date_dir).mkdir(exist_ok= True)
         pathlib.Path(command_dir).mkdir(exist_ok= True)
